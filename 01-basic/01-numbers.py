738bf94b2f0514b81e8753e2d18ec6fbd84341d9
mp5maker <khan.photon@gmail.com>
2025-09-19 15:56:26 +0600

    :memo: doc: added notes on numbers in python

---
+++ 01-basic/01-numbers.py
@@ -0,0 +1,75 @@
+print("int -----------")
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+print(type(2 + 4))
+
+print("     ")
+print("float -----------")
+print("Takes a lot of space in memory")
+print(2.0 + 4.0)
+print("     ")
+print(2.0 - 4.0)
+print(2.0 * 4.0)
+print(2.0 / 4.0)
+print(type(2.0 + 4.0))
+print(2 ** 3)
+print("Floor division" + str(5 // 4))
+print("Modulo (Remainder)" + str(6 % 4))
+print("bin", bin(5))
+print("int from bin", int("0b101", 2))
+
+print("     ")
+print("math functions -----------")
+print(round(3.75))
+print(abs(-3.75))
+print(pow(3, 2))
+print(max(3, 6))
+print(min(3, 6))
+print("     ")
+print("Type conversion -----------")
+print(int(3.75))
+print(float(3))
+print("     ")
+
+print("     ")
+print("augmented assignment operators -----------")
+x = 5
+print(x)
+x += 3  # x = x + 3
+print(x)
+x -= 3  # x = x - 3
+print(x)
+x *= 3  # x = x * 3
+print(x)
+x /= 3  # x = x / 3
+print(x)
+x **= 3  # x = x ** 3
+print(x)
+x //= 3  # x = x // 3
+print(x)
+x %= 3  # x = x % 3
+print(x)
+
+
+print("     ")
+print("operator precedence -----------")
+print((20 - 3) + 2 ** 2)
+
+# ()
+# **
+# *
+# /
+# +
+# -
+
+
+print("     ")
+print("complex numbers -----------")
+print("Has a real and imaginary part")
+print(3 + 4j)
+print(3 + 4j + 1)
+print(3 + 4j * 2)
+print(3 + 4j / 2)
+print(type(3 + 4j))
---
+++ 01-basic/theory.md
@@ -0,0 +1,27 @@
+## Interpreter
+- Basically goes line by line and executes the code
+
+## Compiler
+- Translates the entire code into machine code before execution
+
+## Translators
+- Compiler, Interpreter, Assembler
+- onverts code to machine code
+
+## Fundamental Date Types
+- int
+- float
+- bool
+- str
+- list
+- tuple
+- set
+- dict
+
+## Numbers
+
+- int
+  - check with 'type'
+
+- float
+  - check with 'type'
